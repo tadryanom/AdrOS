ab88cf1754135011bd7a81f5f86f3a047d45ccbf
Tulio Muniz <tadryanom@hotmail.com>
2018-02-15 11:56:40 -0300

    Add files via upload

---
+++ src/kmain.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+int kmain(int i, int j, int k){
+    return i+j+k;
+}
+
