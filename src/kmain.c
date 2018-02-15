fe2f074d39b8162b8af3f8f98721241b0185495a
Tulio Muniz <tadryanom@hotmail.com>
2018-02-15 11:56:40 -0300

    Add files via upload

---
+++ src/kmain.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+int kmain(int i, int j, int k){
+    return i+j+k;
+}
+
