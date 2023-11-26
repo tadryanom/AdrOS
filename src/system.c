e2b4016e7ec140a435fc71e1db97600cd799c4a3
Tulio A M Mendes <tadryanom@hotmail.com>
2023-11-26 06:56:02 -0300

    Adjust of some bits :)

--- src/system.c
+++ src/system.c
@@ -10,11 +10,6 @@
 
 #include <system.h>
 
-void outportb (u16int port, u8int data)
-{
-    asm volatile ("outb %1, %0" : : "dN" (port), "a" (data));
-}
-
 u8int inportb (u16int port)
 {
     u8int ret;
@@ -22,9 +17,21 @@ u8int inportb (u16int port)
     return ret;
 }
 
-u16int inportw(u16int port)
+u16int inportw (u16int port)
 {
     u16int ret;
     asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
     return ret;
 }
+
+void outportb (u16int port, u8int data)
+{
+    asm volatile ("outb %1, %0" : : "dN" (port), "a" (data));
+}
+
+void outportw (u16int port, u16int data)
+{
+    asm volatile ("outw %1, %0" : : "dN" (port), "a" (data));
+}
+
+
