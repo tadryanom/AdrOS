bb99799622131e71dca4b313412add866eaa2a13
Tulio A M Mendes <tadryanom@hotmail.com>
2023-11-27 02:35:27 -0300

    Add GTD, IDT, ISR

--- include/screen.h
+++ include/screen.h
@@ -13,8 +13,8 @@
 
 #include <typedefs.h>
 
-extern void init_video (void);
-extern void cls (void);
-extern void put_char (s8int);
+void init_video (void);
+void cls (void);
+void put_char (s8int);
 
 #endif
