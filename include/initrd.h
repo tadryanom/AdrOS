438fc20596b0c3e05f247df151297f2cd7075620
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-03 15:29:55 +0000

    Feat: Add VFS, InitRD Driver and mkinitrd tool

---
+++ include/initrd.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef INITRD_H
+#define INITRD_H
+
+#include "fs.h"
+#include <stdint.h>
+
+typedef struct {
+    uint8_t magic; // Magic number 0xBF
+    char name[64];
+    uint32_t offset; // Offset relative to start of initrd
+    uint32_t length;
+} initrd_file_header_t;
+
+// Initialize InitRD and return the root node (directory)
+fs_node_t* initrd_init(uint32_t location);
+
+#endif
---
+++ src/kernel/fs.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#include "fs.h"
+
+fs_node_t* fs_root = NULL;
+
+uint32_t vfs_read(fs_node_t* node, uint32_t offset, uint32_t size, uint8_t* buffer) {
+    if (node->read)
+        return node->read(node, offset, size, buffer);
+    return 0;
+}
+
+uint32_t vfs_write(fs_node_t* node, uint32_t offset, uint32_t size, uint8_t* buffer) {
+    if (node->write)
+        return node->write(node, offset, size, buffer);
+    return 0;
+}
+
+void vfs_open(fs_node_t* node) {
+    if (node->open)
+        node->open(node);
+}
+
+void vfs_close(fs_node_t* node) {
+    if (node->close)
+        node->close(node);
+}
