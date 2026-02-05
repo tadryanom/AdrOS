4f3958774b3a50079fec1c532f4956476a8a1995
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-05 02:10:57 -0300

    First test template

---
+++ tests/1-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+# All rights reserved.
+# See LICENSE for details.
+#
+# Source: https://github.com/tadryanom/AdrOS
+#
+
+
+echo "##############################"
+echo "# Testing...                 #"
+echo "##############################"
