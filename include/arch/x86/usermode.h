4e6a0628b3d4e4e401d01be14f50be1bc6ab9a86
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-06 15:09:23 -0300

    x86: stabilize initrd/PMM and ring3 ELF bring-up

---
+++ include/arch/x86/usermode.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ARCH_X86_USERMODE_H
+#define ARCH_X86_USERMODE_H
+
+#include <stdint.h>
+
+#if defined(__i386__)
+__attribute__((noreturn)) void x86_enter_usermode(uintptr_t user_eip, uintptr_t user_esp);
+#endif
+
+#endif
