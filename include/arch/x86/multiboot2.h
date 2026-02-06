15b91627ac803d4cb8247bd6aacde7f2252920aa
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-05 21:53:54 -0300

    x86: fix multiboot2 mmap tag and boot.S physical symbol addressing

--- include/arch/x86/multiboot2.h
+++ include/arch/x86/multiboot2.h
@@ -48,12 +48,19 @@ struct multiboot_tag_basic_meminfo {
     uint32_t mem_upper;
 };
 
+struct multiboot_mmap_entry {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+    uint32_t zero;
+};
+
 struct multiboot_tag_mmap {
     uint32_t type;
     uint32_t size;
     uint32_t entry_size;
     uint32_t entry_version;
-    struct multiboot_mmap_entry entries[0];
+    struct multiboot_mmap_entry entries[];
 };
 
 struct multiboot_tag_module {
@@ -62,13 +69,6 @@ struct multiboot_tag_module {
     uint32_t mod_start;
     uint32_t mod_end;
     char string[0];
-};
-
-struct multiboot_mmap_entry {
-    uint64_t addr;
-    uint64_t len;
-    uint32_t type;
-    uint32_t zero;
 };
 
 #define MULTIBOOT_MEMORY_AVAILABLE              1
