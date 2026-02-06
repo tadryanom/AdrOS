d631f05b24d1c0ee9d88f96321b248455afb04dc
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-06 08:40:33 -0300

    x86/boot: expand early mappings and stabilize higher-half bring-up

--- src/arch/x86/idt.c
+++ src/arch/x86/idt.c
@@ -149,11 +149,8 @@ void idt_init(void) {
 
     // Load IDT
     __asm__ volatile("lidt %0" : : "m"(idtp));
-    
-    // Enable interrupts!
-    __asm__ volatile("sti");
-    
-    uart_print("[IDT] Interrupts Enabled!\n");
+
+    uart_print("[IDT] Loaded.\n");
 }
 
 void register_interrupt_handler(uint8_t n, isr_handler_t handler) {
