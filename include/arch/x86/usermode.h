7c002cd1860ddc189039aed897de5e11d1f5faa9
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-06 15:09:23 -0300

    x86: stabilize initrd/PMM and ring3 ELF bring-up

---
+++ include/arch/x86/usermode.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ARCH_X86_USERMODE_H
+#define ARCH_X86_USERMODE_H
+
+#include <stdint.h>
+
+#if defined(__i386__)
+__attribute__((noreturn)) void x86_enter_usermode(uintptr_t user_eip, uintptr_t user_esp);
+#endif
+
+#endif
--- src/arch/x86/gdt.c
+++ src/arch/x86/gdt.c
@@ -11,6 +11,7 @@
 #include "gdt.h"
 
 #include "uart_console.h"
+ #include "utils.h"
 
 struct gdt_entry {
     uint16_t limit_low;
