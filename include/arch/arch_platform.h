c152017ca7418d1567ff470d4a4c94538e5ce957
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 13:21:47 -0300

    kernel: move arch-specific init into arch_platform hooks

---
+++ include/arch/arch_platform.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ARCH_PLATFORM_H
+#define ARCH_PLATFORM_H
+
+#include "kernel/boot_info.h"
+
+int arch_platform_setup(const struct boot_info* bi);
+int arch_platform_start_userspace(const struct boot_info* bi);
+void arch_platform_usermode_test_start(void);
+
+#endif
