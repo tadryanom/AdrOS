2a80f80e8ef8ee5be21b410f6616d02da0e824de
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 19:13:42 -0300

    fs: add lseek/stat/fstat and minimal errno

---
+++ include/errno.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ERRNO_H
+#define ERRNO_H
+
+#define EPERM 1
+#define ENOENT 2
+#define EIO 5
+#define EBADF 9
+#define EFAULT 14
+#define EINVAL 22
+#define ESPIPE 29
+
+#endif
