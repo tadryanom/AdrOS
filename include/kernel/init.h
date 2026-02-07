0e782cf1ec6b12df2d2a052afab95b43a802d4ca
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 13:27:02 -0300

    kernel: introduce init_start to manage initrd and userspace bring-up

---
+++ include/kernel/init.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef KERNEL_INIT_H
+#define KERNEL_INIT_H
+
+#include "kernel/boot_info.h"
+
+void init_start(const struct boot_info* bi);
+
+#endif
