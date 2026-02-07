9f7a9559b02b9b89ee8308b2ef581632350cecdf
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 17:46:02 -0300

    proc: add waitpid + exit status; add temporary spawn() test helper

--- src/kernel/tty.c
+++ src/kernel/tty.c
@@ -15,6 +15,8 @@
 #include "uart_console.h"
 #include "uaccess.h"
 
+#include "hal/cpu.h"
+
 #define TTY_LINE_MAX 256
 #define TTY_CANON_BUF 1024
 #define TTY_WAITQ_MAX 16
@@ -195,6 +197,8 @@ int tty_read(void* user_buf, uint32_t len) {
         }
 
         spin_unlock_irqrestore(&tty_lock, flags);
+
+        hal_cpu_enable_interrupts();
         schedule();
     }
 }
