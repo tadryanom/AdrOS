5c7850eece02c6534f3abdd47ddd6d8b8df11fb0
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 20:13:26 -0300

    kernel: add console subsystem and kprintf (uart+vga)

---
+++ include/console.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include <stddef.h>
+#include <stdarg.h>
+
+void console_init(void);
+void console_enable_uart(int enabled);
+void console_enable_vga(int enabled);
+
+void console_write(const char* s);
+
+int kvsnprintf(char* out, size_t out_size, const char* fmt, va_list ap);
+int ksnprintf(char* out, size_t out_size, const char* fmt, ...);
+
+void kprintf(const char* fmt, ...);
+
+#endif
