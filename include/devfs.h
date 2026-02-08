53c688981b5930c4aa9215c01033af2da228ccda
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-07 23:55:41 -0300

    posix: devfs with /dev/null and /dev/tty
    Add a minimal devfs mounted at /dev providing /dev/null and /dev/tty char devices. Implement tty_read_kbuf/tty_write_kbuf to support VFS-backed tty IO and allow open/read/write through FS_CHARDEVICE nodes. Add userland smoke tests.

---
+++ include/devfs.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef DEVFS_H
+#define DEVFS_H
+
+#include "fs.h"
+
+fs_node_t* devfs_create_root(void);
+
+#endif
