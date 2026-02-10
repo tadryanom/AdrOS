3ca768889c1ba4487e7b51aec7a33a14cc2bc342
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-09 21:11:50 -0300

    diskfs: add minimal on-disk filesystem mounted at /disk

---
+++ include/diskfs.h
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef DISKFS_H
+#define DISKFS_H
+
+#include "fs.h"
+#include <stdint.h>
+
+fs_node_t* diskfs_create_root(void);
+
+// Open (and optionally create) a diskfs file at the root (flat namespace).
+// rel_path must not contain '/'.
+// flags: supports O_CREAT (0x40) and O_TRUNC (0x200) semantics (minimal).
+int diskfs_open_file(const char* rel_path, uint32_t flags, fs_node_t** out_node);
+
+#endif
