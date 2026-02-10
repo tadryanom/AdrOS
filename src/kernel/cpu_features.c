ac5e10ae003c31ccd4445e20895fb6ba13e964f9
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-10 03:53:50 -0300

    feat: implement CPUID feature detection + HAL wrapper
    Add x86 CPUID detection (src/arch/x86/cpuid.c) that queries:
    - Leaf 0: vendor string, max leaf
    - Leaf 1: feature flags (PAE, NX, APIC, SEP, SSE, SSE2, HTT, etc)
    - Leaf 1 EBX: topology (APIC ID, logical CPUs)
    - Extended leaves: NX, Long Mode, SYSCALL, brand string
    
    HAL wrapper (include/hal/cpu_features.h):
    - hal_cpu_detect_features() / hal_cpu_get_features() / hal_cpu_print_features()
    - x86 impl in src/hal/x86/cpu_features.c
    - Weak default stub in src/kernel/cpu_features.c
    
    Called from kernel_main() right after console_init().
    QEMU output: GenuineIntel, PAE APIC SEP SSE SSE2 FXSR HYPERVISOR.
    
    Passes: make, cppcheck, QEMU smoke test.

---
+++ src/kernel/cpu_features.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#include "hal/cpu_features.h"
+#include "uart_console.h"
+
+#include <stddef.h>
+
+static struct cpu_features g_default_features;
+
+__attribute__((weak))
+void hal_cpu_detect_features(void) {
+    for (size_t i = 0; i < sizeof(g_default_features); i++)
+        ((uint8_t*)&g_default_features)[i] = 0;
+    uart_print("[CPU] No arch-specific feature detection.\n");
+}
+
+__attribute__((weak))
+const struct cpu_features* hal_cpu_get_features(void) {
+    return &g_default_features;
+}
+
+__attribute__((weak))
+void hal_cpu_print_features(void) {
+    uart_print("[CPU] Feature detection not available.\n");
+}
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -31,6 +31,7 @@
 #include "arch/arch_platform.h"
 
 #include "hal/cpu.h"
+#include "hal/cpu_features.h"
 
 
 /* Check if the compiler thinks we are targeting the wrong operating system. */
@@ -44,6 +45,9 @@
  */
 void kernel_main(const struct boot_info* bi) {
     console_init();
+
+    hal_cpu_detect_features();
+    hal_cpu_print_features();
 
     kprintf("[AdrOS] Initializing PMM...\n");
     
