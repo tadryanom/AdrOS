5b1578c25a6891ae8f738054adf996274369aff4
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-11 23:36:01 -0300

    feat: ulibc signal.h with raise(), kill(), sigprocmask() and POSIX signal constants

---
+++ user/ulibc/src/signal.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * AdrOS Kernel - Copyright (c) 2018, Tulio A M Mendes <myself@tadryanom.me>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://projects.tadryanom.me/?p=AdrOS.git;a=summary
+ * Mirror: https://github.com/tadryanom/AdrOS
+ */
+
+#include "signal.h"
+#include "syscall.h"
+#include "errno.h"
+
+int kill(int pid, int sig) {
+    return __syscall_ret(_syscall2(SYS_KILL, pid, sig));
+}
+
+int raise(int sig) {
+    return kill(_syscall0(SYS_GETPID), sig);
+}
+
+int sigprocmask(int how, const uint32_t* set, uint32_t* oldset) {
+    return __syscall_ret(_syscall3(SYS_SIGPROCMASK, how, (int)set, (int)oldset));
+}
