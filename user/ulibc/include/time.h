231235d03f89f22634874bd6f395b432ac0b7917
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-12 04:36:52 -0300

    feat: ulibc extensions for DOOM — mmap, munmap, ioctl, nanosleep, clock_gettime
    Added userspace wrappers required for the DOOM port:
    - sys/mman.h + mman.c: mmap() and munmap() for framebuffer mapping
    - sys/ioctl.h + ioctl.c: ioctl() for framebuffer info queries
    - time.h + time.c: nanosleep() and clock_gettime() for frame timing
    
    All wrappers use the existing INT 0x80 syscall interface.

---
+++ user/ulibc/include/time.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ULIBC_TIME_H
+#define ULIBC_TIME_H
+
+#include <stdint.h>
+
+struct timespec {
+    uint32_t tv_sec;
+    uint32_t tv_nsec;
+};
+
+#define CLOCK_REALTIME  0
+#define CLOCK_MONOTONIC 1
+
+int nanosleep(const struct timespec* req, struct timespec* rem);
+int clock_gettime(int clk_id, struct timespec* tp);
+
+#endif
---
+++ user/ulibc/src/ioctl.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#include "sys/ioctl.h"
+#include "syscall.h"
+#include "errno.h"
+
+int ioctl(int fd, unsigned long cmd, void* arg) {
+    return __syscall_ret(_syscall3(SYS_IOCTL, fd, (int)cmd, (int)arg));
+}
