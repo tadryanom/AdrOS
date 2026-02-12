3685b20cacbdc4578abaff9b26844cdd65ba29f8
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-11 23:36:01 -0300

    feat: ulibc signal.h with raise(), kill(), sigprocmask() and POSIX signal constants

---
+++ user/ulibc/src/signal.c
@@ -0,0 +1,15 @@
+#include "signal.h"
+#include "syscall.h"
+#include "errno.h"
+
+int kill(int pid, int sig) {
+    return __syscall_ret(_syscall2(SYS_KILL, pid, sig));
+}
+
+int raise(int sig) {
+    return kill(_syscall0(SYS_GETPID), sig);
+}
+
+int sigprocmask(int how, const uint32_t* set, uint32_t* oldset) {
+    return __syscall_ret(_syscall3(SYS_SIGPROCMASK, how, (int)set, (int)oldset));
+}
