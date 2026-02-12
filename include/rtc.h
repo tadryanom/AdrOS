3d07c7fe2ccb1da483b123a4b6314b2a5b12dea6
Tulio A M Mendes <tadryanom@hotmail.com>
2026-02-12 00:20:48 -0300

    feat: RTC driver (CMOS real-time clock) + clock_gettime(CLOCK_REALTIME) uses wall-clock time

---
+++ include/rtc.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef RTC_H
+#define RTC_H
+
+#include <stdint.h>
+
+struct rtc_time {
+    uint8_t second;
+    uint8_t minute;
+    uint8_t hour;
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+};
+
+void rtc_init(void);
+void rtc_read(struct rtc_time* t);
+uint32_t rtc_unix_timestamp(void);
+
+#endif
