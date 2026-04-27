21309b3b0658f00b2ea666102244d9508b5c6605
Tulio A M Mendes <tadryanom@hotmail.com>
2026-04-26 23:34:15 -0300

    userland: enhance 17 utilities and ulibc with POSIX features
    ulibc:
    - Add sys/statvfs.h and statvfs() implementation
    - Extend time.h: strftime, localtime, gmtime, ctime, asctime, difftime
    - Fix time_t conflict (was uint32_t in time.h, int32_t in sys/types.h)
    - Add unistd.h include to statvfs.c for close()
    
    High-priority commands:
    - awk: BEGIN/END blocks, NF/NR vars, print with OFS, -v var=val, /bin/bash
    - find: -mtime, -size, -perm, -user, -maxdepth, -delete, -exec
    - chmod: symbolic mode support (u+x, go-w, a=rw)
    - grep: BRE/ERE regex, -i, -r, -l, -q, -n flags
    - sed: d/p/q/a/i/c/y commands, line/regex addresses, -n mode
    - ps: BSD-style output with UID/PID/PPID/STAT/TIME/CMD
    - top: system summary header, process list with -n iterations
    
    Medium-priority commands:
    - sh: command substitution $(...), tilde expansion ~, $$/$! vars,
      variable overflow protection
    - cp/mv: preserve source file permissions via fstat
    - rm: proper -rf recursive directory removal with getdents
    - kill: -l flag to list signal names
    - init: SIGUSR1/SIGUSR2 handlers for poweroff/reboot
    
    Low-priority commands:
    - stat: date/time display, UID/GID name lookup, file type
    - dd: conv= (ucase/lcase/swab/noerror/sync/trunc), skip=, seek=
    - ls: -n flag for numeric UID/GID
    
    Tests: 120/120 QEMU, 33/33 battery, 57/57 host (3 skipped)

---
+++ user/ulibc/include/sys/statvfs.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2018, Tulio A M Mendes <tadryanom@hotmail.com>
+ * All rights reserved.
+ * See LICENSE for details.
+ *
+ * Source: https://github.com/tadryanom/AdrOS
+ */
+
+#ifndef ULIBC_SYS_STATVFS_H
+#define ULIBC_SYS_STATVFS_H
+
+#include <sys/types.h>
+
+struct statvfs {
+    unsigned long f_bsize;    /* Filesystem block size */
+    unsigned long f_frsize;   /* Fragment size */
+    unsigned long f_blocks;   /* Size of fs in f_frsize units */
+    unsigned long f_bfree;    /* Number of free blocks */
+    unsigned long f_bavail;   /* Free blocks for unprivileged users */
+    unsigned long f_files;    /* Number of inodes */
+    unsigned long f_ffree;    /* Number of free inodes */
+    unsigned long f_favail;   /* Free inodes for unprivileged users */
+    unsigned long f_fsid;     /* Filesystem ID */
+    unsigned long f_flag;     /* Mount flags */
+    unsigned long f_namemax;  /* Maximum filename length */
+};
+
+int statvfs(const char* path, struct statvfs* buf);
+
+#endif
